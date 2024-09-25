cf1912b16fd9bd30b7994d6688e6be2970e03e52
Louisewindt <147713047+Louisewindt@users.noreply.github.com>
2024-09-25 10:29:42 +0200

    dfdf

---
+++ script.js
@@ -0,0 +1,6 @@
+    // Go back to the previous page when "Tilbage" button is clicked
+    document.querySelector('.page-header__back-button').addEventListener('click', function() {
+        history.back();
+    });
+
+    
